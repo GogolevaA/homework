1e7ecea060433204faf592897f00b7e464bae30a
lisssenok222 <brokolinka222@yandex.ru>
2022-08-20 20:14:48 +0300

    задача c семинара

---
+++ lesson1/task5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число:");
+int a; //init
+a = Convert.ToInt32(Console.ReadLine()); //присвоить
+while (a != 0)
+{
+    Console.Write(a%10+ ",\n");
+    a = a/10;
+}
