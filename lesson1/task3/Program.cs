2a3b99a80c1fdf484fdca14ce66dd0f887a4dca1
lisssenok222 <brokolinka222@yandex.ru>
2022-08-20 19:11:01 +0300

    задача 3 завершена

---
+++ lesson1/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое положительное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 != 0)
+{
+    Console.WriteLine("Число НЕчетное");
+}
+else 
+{
+     Console.WriteLine("число четное");
+}
+
