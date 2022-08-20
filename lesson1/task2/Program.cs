cfe6ae410a9089e1e95b8ed1f9f4d5b4a42341ba
lisssenok222 <brokolinka222@yandex.ru>
2022-08-20 18:52:54 +0300

    задача 2 завершена

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a>=b && a>=c)
+{
+    Console.WriteLine("Число а максимальное");
+}
+else if(b>=a && b>=c)
+{
+    Console.WriteLine("Число b максимальное");
+}
+else if(c>=a && c>=b)
+{
+    Console.WriteLine("Число c максимальное");
+}
