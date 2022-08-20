69dd8a25863c8a721824b2c8e959922046324796
lisssenok222 <brokolinka222@yandex.ru>
2022-08-20 18:43:12 +0300

    задача 1 завершена

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    Console.WriteLine("Числоа а максимальное");
+}
+else
+{
+    Console.WriteLine("Числоа b максимальное");
+}
