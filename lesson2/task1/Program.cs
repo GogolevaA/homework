b2a07fd244a14c3ef15d43d81af264f8349cdb5b
lisssenok222 <brokolinka222@yandex.ru>
2022-08-23 20:24:05 +0300

    1 задача

---
+++ lesson2/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// a = int(input('Enter 1st number: '))
+// a = a//10
+// a = a % 10
+
+
+// print({a})
+
+Console.Write("Введите трехзначное число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+int  x = a / 10;
+int  b = x % 10;
+Console.WriteLine("второе число-" + b);
+
+// Console.Write("Введи трёхзначное число: ");
+// int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// string stringNumber = Convert.ToString(threeDigitNumber);
+// Console.WriteLine("вторая цифра этого числа -> "+stringNumber[2]);
