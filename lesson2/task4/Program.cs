1602e2c7182bc393f5ad1b136a7c4a2d916cae27
lisssenok222 <brokolinka222@yandex.ru>
2022-08-23 21:06:32 +0300

    4 задача

---
+++ lesson2/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите введите число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a%10==1)
+{
+    Console.WriteLine(a + "  програмист");
+}
+else if (a%10==2 || a%10==3 || a%10==4 )
+{
+     Console.WriteLine(a + "  програмиста");
+}
+else 
+{
+     Console.WriteLine(a + "  програмистов");
+}
