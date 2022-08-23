851c60dd1107aab1bb43a8d0cba2d257ca3b02c9
lisssenok222 <brokolinka222@yandex.ru>
2022-08-23 20:58:37 +0300

    3 задача

---
+++ lesson2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите введите число от 1 до 7 а:");
+int a = Convert.ToInt32(Console.ReadLine());
+// int b = 6;
+// int c = 7;
+if(a< 6 && a> 0)
+{
+    Console.WriteLine("работаем");
+}
+else 
+{
+    Console.WriteLine("выходной");
+}
