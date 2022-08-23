2764bc32fc8c60f90118569f23e8c8cb4fe9f3c3
lisssenok222 <brokolinka222@yandex.ru>
2022-08-23 20:37:41 +0300

    2 задача

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число а:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<100)
+{
+    Console.WriteLine("Третьей цыфры нет");
+}
+else 
+{
+    Console.WriteLine(a%10);
+}
