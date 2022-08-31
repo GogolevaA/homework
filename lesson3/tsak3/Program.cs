25e9caadac26cfea3c8251db526f3ae48a3247fc
lisssenok222 <brokolinka222@yandex.ru>
2022-09-01 00:17:21 +0300

    3 задача

---
+++ lesson3/tsak3/Program.cs
@@ -0,0 +1,12 @@
+﻿// string cub3();
+// {
+    Console.WriteLine("Введите положительное число");
+    int N = Convert.ToInt32(Console.ReadLine());
+    string res = "";
+    for ( int i = 1; i <= N; i++)
+    {
+      res = res + i*i*i+",";
+    }
+// return res;
+// Console.WriteLine(cub3());
+Console.WriteLine(res);
