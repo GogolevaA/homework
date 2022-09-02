9b38c4446c3760d30bf59ffdf91dee7e361d9f93
lisssenok222 <brokolinka222@yandex.ru>
2022-09-02 21:20:56 +0300

    2 задача

---
+++ lesson4/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int Factorial(int N);
+{
+    int sum =0;
+        for ( int i = 0; i <= N; i++)
+        {
+            i=N%10;
+            N=N/10;
+            sum +=i;
+        }
+    return sum;
+}
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(N));
